f4f4963dd497812e00a2fa673b1289a55a96da2a
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-02-21 10:37:34 -0700

    Sections

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+#---------- START OF AVERY'S CODE (Retrieve user info) ----------
 
+#---------- END OF AVERY'S CODE (Retrieve user info) ----------
+
+#---------- START OF YENESIS' CODE (User profiles) ----------
+
+#---------- END OF YENESIS' CODE (User profiles) ----------
+
+#---------- START OF AVERY'S CODE (Leaderboard) ----------
+
+#---------- END OF AVERY'S CODE (Leaderboard) ----------
+
+#---------- START OF JONAS' CODE (Number guessing game) ----------
+
+#---------- END OF JONAS' CODE (Number guessing game) ----------
+
+#---------- START OF AVERY'S CODE (Main function and saving scores) ----------
+
+#---------- END OF AVERY'S CODE (Main function and saving scores) ----------
