f8e098a8fb90bfb81fc959a0d186fbb4729d4a63
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-02-24 10:50:46 -0700

    Folders and game update

---
+++ game.py
@@ -0,0 +1,35 @@
+#---------- START OF JONAS' CODE (Number guessing game) ----------
+
+def play_game(): #Master function for the game part of the program.
+	num = random.randint(1, 100)
+	score = 10
+	while score != 0: #If the user hasn't lost yet
+		while True:
+			try:
+				print(f"You have {score} tries left.")
+				guess = int(input("What is your guess?: ")) #Get the user's guess
+				break
+			except:
+				print("That's not a valid input. Try again.")
+		if guess == num: #Tell the user if their guess is too small or large.
+			break
+		elif guess < num:
+			print("Your guess was too small.")
+			score -= 1
+		elif guess > num:
+			print("Your guess was too large.")
+			score -= 1
+	
+	if user_info:
+		if score == 0:
+			print("Looks like you lost. Better luck next time!")
+		else:
+			print(f"Nice job! Your final score was: {score}")
+			user_info["scores"].append(score)
+		return user_info
+	else:
+		print("You're not logged in, so your score won't be saved.")	
+	
+	display_leaderboard()
+
+#---------- END OF JONAS' CODE (Number guessing game) ----------
--- main.py
+++ main.py
@@ -4,82 +4,10 @@ import random
 import csv
 import os
 
-#---------- START OF AVERY'S CODE (Retrieve user info) ----------
+#---------- START OF AVERY'S CODE (Main function, saving and retrieving scores) ----------
 
 
 
-#---------- END OF AVERY'S CODE (Retrieve user info) ----------
-
-#---------- START OF YENESIS' CODE (User profiles) ----------
 
 
-
-#---------- END OF YENESIS' CODE (User profiles) ----------
-
-#---------- START OF AVERY'S CODE (Leaderboard) ----------
-
-
-
-#---------- END OF AVERY'S CODE (Leaderboard) ----------
-
-#---------- START OF JONAS' CODE (Number guessing game) ----------
-
-#Assume that users is a list of dictionaries, each containing a name (user[“name”]) and a list of scores (user[“scores”]).
-#Assume that user_info is the user in users that is currently logged in.
-#display_leaderboard() is a function that displays the entire leaderboard.
-#These values are not defined in this part of the program.
-
-def play_game():
-	num = random.randint(1, 100)
-	score = 10
-	while score != 0:
-		while True:
-			try:
-				guess = int(input("What is your guess?: "))
-				break
-			except:
-				print("That's not a valid input. Try again.")
-		if guess == num:
-			break
-		elif guess < num:
-			print("Your guess was too small.")
-			score -= 1
-		elif guess > num:
-			print("Your guess was too large.")
-			score -= 1
-	if user_info:
-		if score == 0:
-			print("Whoops! You lost.")
-		else:
-			print(f"Nice job! Your final score was: {score}")
-			user_info["scores"].append(score)
-		return user_info
-			
-			
-    
-    if user_info:
-        if score == 0:
-		    Tell user they lost
-		    Return user_info
-	    ELSE:
-		    Congratulate user
-		    Add score to user_info
-		Return user info
-	ELSE:
-		Tell the user that their score wasn’t saved, since they aren’t logged in.
-	
-display_leaderboard()
-    
-    
-    
-
-#play_game is called in the main function.
-
-
-#---------- END OF JONAS' CODE (Number guessing game) ----------
-
-#---------- START OF AVERY'S CODE (Main function and saving scores) ----------
-
-
-
-#---------- END OF AVERY'S CODE (Main function and saving scores) ----------
+#---------- END OF AVERY'S CODE (Main function, saving and retrieving scores) ----------
